ed0a8b959b5fe282c896d23417b4f1dd707248ca
Maor Rocky <maorrocky@gmail.com>
2019-11-18 14:28:53 +0200

    Add files via upload

--- reader.ml
+++ reader.ml
@@ -4,7 +4,7 @@ open PC;;
 
 exception X_not_yet_implemented;;
 exception X_this_should_not_happen;;
-  
+
 type number =
   | Int of int
   | Float of float;;
@@ -38,26 +38,42 @@ let rec sexpr_eq s1 s2 =
 let nt_boolean = 
   let nt = disj (word_ci "#f") (word_ci "#t") in
   let last_nt s = (match (nt s) with
-          | (['#'; 'f'], _) -> Bool(false)
-          | (['#'; 't'], _) -> Bool(true)
-          | (['#'; 'T'], _) -> Bool(true)
-          | (['#'; 'F'], _) -> Bool(false)
-          | _ -> raise X_no_match) in 
+      | (['#'; 'f'], _) -> Bool(false)
+      | (['#'; 't'], _) -> Bool(true)
+      | (['#'; 'T'], _) -> Bool(true)
+      | (['#'; 'F'], _) -> Bool(false)
+      | _ -> raise X_no_match) in 
+  last_nt;;
+
+let visible_simple_char_nt = 
+  fun ch ->
+  if ((int_of_char ch)>32)
+  then Char(ch) 
+  else raise X_no_match;;
+
+let named_char_nt =
+  let nt = disj_list [(word_ci "nul");(word_ci "newline") ;(word_ci "return");(word_ci "tab");
+  (word_ci "page");(word_ci "space")] in
+  let last_nt s = (match (nt s) with
+      | (['n'; 'u';'l'], _) -> Char(char_of_int 0)
+      | (['n'; 'e';'w';'l';'i';'n';'e'], _) -> Char(char_of_int 10)
+      | (['r'; 'e';'t';'u';'r';'n'], _) -> Char(char_of_int 13)
+      | (['t'; 'a';'b'], _) -> Char(char_of_int 9)
+      | (['p';'a';'g';'e'], _) -> Char(char_of_int 12)
+      | (['s';'p';'a';'c';'e'], _) -> Char(char_of_int 32)
+      | _ -> raise X_no_match) in 
   last_nt;;
 
 
 let charPrefixnt = 
   let nt_cp = (word_ci "#\\") in
-    let last_nt s = (match (nt_cp s) with
-    |(['#';'\\'],[c]) -> ( visible_simple_char_nt c)
-    | _ -> raise X_no_match) in 
+  let comb_nt = disj named_char_nt nt_whitespace
+  let last_nt s = (match (nt_cp s) with
+      |(['#';'\\'],[c]) -> (comb_nt c)
+      | _ -> raise X_no_match) in 
   last_nt;;
 
-let visible_simple_char_nt = 
-  fun ch ->
-    if ((int_of_char ch)>32)
-    then Char(ch) 
-    else raise X_no_match;;
+
 
 let make_paired nt_left nt_right nt =
   let nt = caten nt_left nt in
@@ -69,11 +85,11 @@ let make_paired nt_left nt_right nt =
 let nt_whitespace = const (fun ch -> ch <= ' ');;
 
 let nt_char = 
-let nt_c = disj (char 'A') (char 'B') in
-let nt s = (match (nt_c s) with
+  let nt_c = disj (char 'A') (char 'B') in
+  let nt s = (match (nt_c s) with
       |('A',_)-> char('A')
-| _ -> raise X_no_match)in nt;; 
-    
+      | _ -> raise X_no_match)in nt;; 
+
 let nt_whitespaces= star(char ' ' );;
 
 let make_spaced nt = make_paired nt_whitespaces nt_whitespaces nt;;
@@ -83,17 +99,17 @@ module Reader: sig
   val read_sexprs : string -> sexpr list
 end
 = struct
-let normalize_scheme_symbol str =
-  let s = string_to_list str in
-  if (andmap
-	(fun ch -> (ch = (lowercase_ascii ch)))
-	s) then str
-  else Printf.sprintf "|%s|" str;;
+  let normalize_scheme_symbol str =
+    let s = string_to_list str in
+    if (andmap
+          (fun ch -> (ch = (lowercase_ascii ch)))
+          s) then str
+    else Printf.sprintf "|%s|" str;;
 
 
-let read_sexpr string = raise X_not_yet_implemented ;;
+  let read_sexpr string = raise X_not_yet_implemented ;;
 
-let read_sexprs string = raise X_not_yet_implemented;;
+  let read_sexprs string = raise X_not_yet_implemented;;
 
 
 
