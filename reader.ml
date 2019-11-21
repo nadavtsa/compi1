8161894b251cfa90a0e7d5dd7df1c81b0af00ac9
Maor Rocky <maorrocky@gmail.com>
2019-11-21 21:41:13 +0200

    Add files via upload
    string completed

--- reader.ml
+++ reader.ml
@@ -34,44 +34,6 @@ let rec sexpr_eq s1 s2 =
   | TagRef(name1), TagRef(name2) -> name1 = name2
   | _ -> false;;
 (* MAOR *)
-let boolean_parser = 
-  let nt = disj (word_ci "#f") (word_ci "#t")  in
-  let last_nt  = pack nt (fun (prefix) -> match((list_to_string prefix)) with
-      |  "#f" -> Bool(false)
-      |  "#F" -> Bool(false)
-      | "#t" -> Bool(true)
-      | "#T" -> Bool(true)
-      | _ -> raise X_no_match) in 
-  last_nt;;
-
-(* MAOR *)
-let symbol_parser = 
-let symbol_list_nt = disj_list [(word_ci "!");(word_ci "$");(word_ci "^");
-  (word_ci "*");(word_ci "-");(word_ci "_");(word_ci "=");(word_ci "+");(word_ci "<")
-  ;(word_ci "<");(word_ci "?");(word_ci "/");(word_ci ":")] in
-let a_z_nt = range_ci 'A' 'z' in
-let temp_a_z_nt = pack (caten a_z_nt nt_epsilon) (fun (e,s) -> e::s)in
-let numbers_range_nt = range_ci '0' '9' in
-let temp_numbers_range_nt = pack (caten numbers_range_nt nt_epsilon) (fun (e,s) -> e::s)in
-let symbol_disj_nt = disj_list [symbol_list_nt;temp_a_z_nt;temp_numbers_range_nt] in
-let sym_parse = pack symbol_disj_nt (fun (prefix) -> match ((list_to_string prefix)) with
-    | "!" -> Symbol "!"
-| "$" -> Symbol "$"
- | "^"-> Symbol "^"
-| "*"-> Symbol "*"
-|"-" -> Symbol "-"
-|"_" -> Symbol "_"
-|"=" -> Symbol "="
-|"+" -> Symbol "+" 
-|"<" -> Symbol "<"
-|">" -> Symbol ">"
-|"?" -> Symbol "?"
-|"/" -> Symbol "/" 
-|":" -> Symbol ":"
-| _ -> raise X_no_match) in sym_parse ;;
-
-
-
 let make_paired nt_left nt_right nt =
   let nt = caten nt_left nt in
   let nt = pack nt (function (_, e) -> e) in
@@ -80,6 +42,38 @@ let make_paired nt_left nt_right nt =
   nt;;
 
 let nt_whitespace = const (fun ch -> ch <= ' ');;
+
+let nt_whitespaces= star(char ' ' );;
+let nt_comments = star(char ';');;
+
+let make_spaced nt = make_paired nt_whitespaces nt_whitespaces nt;;
+let boolean_parser = 
+  let nt = disj (word_ci "#f") (word_ci "#t")  in
+  let last_nt  = pack nt (fun (prefix) -> match((list_to_string prefix)) with
+      |  "#f" -> Bool(false)
+      |  "#F" -> Bool(false)
+      | "#t" -> Bool(true)
+      | "#T" -> Bool(true)
+      | _ -> raise X_no_match) in 
+  let last_nt = make_spaced last_nt in
+  last_nt;;
+
+
+(* MAOR *)
+let symbol_parser = 
+let symbol_list_nt = disj_list [(word_ci "!");(word_ci "$");(word_ci "^");
+  (word_ci "*");(word_ci "-");(word_ci "_");(word_ci "=");(word_ci "+");(word_ci "<")
+  ;(word_ci "<");(word_ci "?");(word_ci "/");(word_ci ":")] in
+let a_z_nt_capital = plus(range_ci 'A' 'Z') in
+let a_z_nt = plus(range_ci 'a' 'z') in
+let a_z_nt = pack (disj a_z_nt_capital a_z_nt) (List.map lowercase_ascii)in
+let numbers_range_nt = plus(range_ci '0' '9') in
+let symbol_disj_nt = plus (disj_list [symbol_list_nt;a_z_nt;numbers_range_nt]) in
+let symbol_disj_nt = pack symbol_disj_nt (fun (list_of_lists) ->
+                                          List.flatten list_of_lists) in
+let sym_parse = pack symbol_disj_nt (fun (prefix) ->  (Symbol(list_to_string prefix))) in sym_parse;;
+
+
 (* MAOR *)
 let char_parser = 
 (* parser for chars which are greater than space in ASCII *)
@@ -102,25 +96,25 @@ let char_nt2 = pack char_nt (fun (prefix,rest) -> match ((list_to_string prefix)
     | (_, _) -> raise X_no_match) in char_nt2;;
 
 
-let nt_whitespaces= star(char ' ' );;
-let nt_comments = star(char ';');;
-let make_spaced nt = make_paired nt_whitespaces nt_whitespaces nt;;
 
 
 (* string MAOR *)
 (* special nots *)
 
-let meta_nt = disj_list [(word_ci "\r");(word_ci "\n");(word_ci "\t");(word_ci "\f");
-(word_ci "\\");(word_ci "\"")]
+let meta_nt = disj_list [(word_ci "\\r");(word_ci "\\n");(word_ci "\\t");(word_ci "\\f");
+(word_ci "\\\\");(word_ci "\"")]
 let string_Literal_Char_nt = const (fun ch -> ch != '\"' && ch != '\\');;
 let sting_temp_nt = pack (caten string_Literal_Char_nt nt_epsilon) (fun (e,s) -> e::s)
-let stringChar_nt = disj meta_nt sting_temp_nt;;
-let string_without_quotes = make_paired (char '\"') (char '\"') stringChar_nt;;
-let string_pareser = star string_without_quotes;;
+let stringChar_nt = star(disj meta_nt sting_temp_nt);;
+let star_stringChar_nt = pack stringChar_nt (fun (list_of_lists) ->
+                                          List.flatten list_of_lists);;
+let string_parser = pack star_stringChar_nt (fun nadav -> String(list_to_string nadav)) 
 
-let tok_lparen =let lp = char '(' in
-  let spaced= caten(caten nt_whitespaces lp) nt_whitespaces in
-  pack spaced (fun ((l, p), r) -> p);;
+
+
+
+
+
 
 module Reader: sig
   val read_sexpr : string -> sexpr
